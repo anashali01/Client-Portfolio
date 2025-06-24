d05c83c93df3ba0d5a280a27b7244d2c4fc486e1
Daxrokz10 <dakshgagnani@gmail.com>
2025-06-24 22:07:59 +0530

    graphic designs done

--- Designs/Graphic Design/js/script.js
+++ Designs/Graphic Design/js/script.js
@@ -3,9 +3,11 @@ const slides = document.querySelectorAll('.slide');
 const indicators = document.querySelectorAll('.indicator');
 const slides2 = document.querySelectorAll('.slide2');
 const slides3 = document.querySelectorAll('.slide3');
+const slides4 = document.querySelectorAll('.slide4');
 var counter = 0;
 var counter2 = 0;
 let counter3 = 0;
+let counter4 = 0;
 
 slides.forEach(
     (slide, index) => {
@@ -18,6 +20,11 @@ slides2.forEach(
     }
 );
 slides3.forEach(
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`;
+    }
+);
+slides4.forEach(
     (slide, index) => {
         slide.style.left = `${index * 100}%`;
     }
@@ -93,6 +100,28 @@ function slideEco() {
     slides3.forEach(
         (slide) => {
             slide.style.transform = `translateX(-${counter3 * 100}%)`;
+        }
+    );
+}
+
+function MorePrev() {
+    counter4--;
+    if (counter4 < 0) {
+        counter4 = slides4.length - 1;
+    }
+    slideMore();
+}
+function MoreNext() {
+    counter4++;
+    if (counter4 >= slides4.length) {
+        counter4 = 0;
+    }
+    slideMore();
+}
+function slideMore() {
+    slides4.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter4 * 100}%)`;
         }
     );
 }
